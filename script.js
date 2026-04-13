f86c5acd7b2b2a42d579aa9d3b03c3a8009040d7
Sopuru <platoon1277@gmail.com>
2026-04-13 16:31:56 +0100

    the raw file was implimented for dates and time object

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
 // console.log("hey");
-// const button = document.querySelector(".button");
+const button = document.querySelector(".button");
 
 // // localStorage.removeItem('sharedData')
 // console.log(receivedData);
 // button.addEventListener("click", moveAll);
+
+console.log("hello");
+
+button.addEventListener("click", (e) => {
+  e.defaultPrevented;
+  const date = document.getElementById("date").value;
+  const time = document.getElementById("time").value;
+
+  const dateValue = new Date(date);
+  const timeValue = new Date(`1994-12-03T${time}`);
+
+  const isoDate = dateValue.toDateString();
+  const isoTime = timeValue.toLocaleTimeString();
+
+  console.log("Date:", isoDate);
+  console.log("time:", isoTime);
+});
