f0bba5b03705a7f7b8442198a2e64daf49bb20bd
Sopuru <platoon1277@gmail.com>
2026-04-14 22:30:21 +0100

    updated

--- script.js
+++ script.js
@@ -1,23 +1,4 @@
-// console.log("hey");
-const button = document.querySelector(".button");
-
-// // localStorage.removeItem('sharedData')
-// console.log(receivedData);
-// button.addEventListener("click", moveAll);
-
-console.log("hello");
-
-button.addEventListener("click", (e) => {
-  e.defaultPrevented;
-  const date = document.getElementById("date").value;
-  const time = document.getElementById("time").value;
-
-  const dateValue = new Date(date);
-  const timeValue = new Date(`1994-12-03T${time}`);
-
-  const isoDate = dateValue.toDateString();
-  const isoTime = timeValue.toLocaleTimeString();
-
-  console.log("Date:", isoDate);
-  console.log("time:", isoTime);
-});
+// // console.log("hey");
+// const button = document.querySelector(".button");
+// const receivedDate = document.querySelector(".received-Date");
+// const receivedTime = document.quer
